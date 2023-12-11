a644518ea0b68ac98a5ce53b1a8fa9ac7236a5b1
erol911 <erolzcn@hotmail.com>
2023-12-12 01:35:04 +0300

    add paging

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css'
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 5;
   
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
@@ -15,9 +17,32 @@ function App() {
     try {
       const response = await axios.get(`https://api.github.com/search/users?q=${searchQuery}`);
       setUsers(response.data.items);
+      console.log(response.data.items)
     } catch (error) {
       console.error('Error fetching data: ', error);
     }
+  };
+
+  const totalPages = Math.ceil(users.length / usersPerPage);
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+      <button key={i} onClick={() => setCurrentPage(i)}>{i}</button>
+    );
+  }
+
+  return pageNumbers;
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
   };
 
   return (
@@ -32,14 +57,22 @@ function App() {
         <button type="submit">Search</button>
       </form>
       <div>
-        {users.map(user => (
-          <div key={user.id}>
-            <img src={user.avatar_url} alt={user.login} width="50" height="50" />
-            <a href={user.html_url} target="_blank" rel="noopener noreferrer">{user.login}</a>
-          </div>
-        ))}
-      </div>
+      {users.slice((currentPage - 1) * usersPerPage, currentPage * usersPerPage).map(user => (
+        <div key={user.id}>
+          <img src={user.avatar_url} alt={user.login} width="75" height="75" />
+          <a href={user.html_url} target="_blank" rel="noopener noreferrer">{user.login}</a>
+        </div>
+      ))}
     </div>
+    <br />
+    <p className='list-user'>{users.length} account found!</p>
+    <div>
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>Prev</button>
+      {renderPageNumbers()}
+      <button onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
+    </div>
+    </div>
+    <br/>
     <footer className=''>Bu uygulama sevgili arkadaşım Oktay Hızarcı'ya ithafen yapılmıştır.</footer></>
   );
 }
